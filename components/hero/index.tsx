1de16debb39576afc178e348fcced33ef9a0f673
Pamela Bortnick <pbortnick@gmail.com>
2023-01-22 19:46:47 -0500

    style fixes

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -3,13 +3,15 @@ import s from './s.module.css'
 const Hero = () => {
   return (
     <header className={s.hero}>
-      <div className={s.text}>
-        <h1 className={s.heading}>
-          <span>Pamela</span>
-          <span>Honey</span>
-          <span>Bortnick</span>
-        </h1>
-        <p className={s.description}>software engineer</p>
+      <div className={s.textContainer}>
+        <div className={s.text}>
+          <h1 className={s.heading}>
+            <span>Pamela</span>
+            <span>Honey</span>
+            <span>Bortnick</span>
+          </h1>
+          <p className={s.description}>software engineer</p>
+        </div>
       </div>
       <div className={s.imageContainer}>
         <Image src={require('./profile-pic.png')} height={800} alt="profile picture of pam" width={660} className={s.image} />
