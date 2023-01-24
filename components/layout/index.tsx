bb951f5965ddc963bb7ef29ad0e1d24a38c589ea
Pamela Bortnick <pbortnick@gmail.com>
2023-01-24 17:50:00 -0500

    add og image

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -4,12 +4,25 @@ import Head from 'next/head'
 import s from './s.module.css'
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const pageTitle = 'Pamela Bortnick'
+  const pageDescription = 'Full stack software engineer'
   return (
     <>
       <Head>
-        <title>Pamela Bortnick</title>
-        <meta name="description" content="Full stack software engineer" />
+        <title>{pageTitle}</title>
+        <meta name="description" content={pageDescription}/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="My page title" key="title" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={pageDescription} />
+        <meta
+          property="twitter:card"
+          content="summary_large_image"
+        />
+        <meta
+          property="og:image"
+          content="https://pamelahoneybortnick.com/og-image.png"
+        />
       </Head>
       <div className={s.wip}>
         <p className={s.wipText}>
