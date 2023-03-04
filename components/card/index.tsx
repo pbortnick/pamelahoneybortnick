0f4e375ce267b8e1163ba2fd130201f61be7065a
Pamela Bortnick <pbortnick@gmail.com>
2023-03-04 02:23:16 -0500

    update icons and focus

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,40 +1,56 @@
-import Link from 'next/link'
-import Image, { type ImageProps } from 'next/legacy/image'
-import classNames from 'classnames'
-import Tag, { type TagProps } from 'components/tag'
-import s from './s.module.css'
+import Link from "next/link";
+import Image, { type ImageProps } from "next/legacy/image";
+import classNames from "classnames";
+import Tag, { type TagProps } from "components/tag";
+import s from "./s.module.css";
 
 export interface CardProps {
-  thumbnail: ImageProps
-  heading: string
-  description?: string
-  tags?: Array<TagProps>
-  url: string
-  className?: string
+	thumbnail: ImageProps;
+	heading: string;
+	description?: string;
+	tags?: Array<TagProps>;
+	url: string;
+	className?: string;
 }
 
-export default function Card({ thumbnail, heading, description, tags, url, className }: CardProps) {
-  return (
-    <div className={classNames(s.card, className)}>
-      <div className={s.image}>
-        <Image
-          {...thumbnail}
-          width={800}
-          height={450}
-          layout="responsive"
-          objectFit="cover"
-        />
-      </div>
-      <div className={s.body}>
-        <h2 className={s.cardTitle}>{heading}</h2>
-        {description && <p className={s.description}>{description}</p>}
-        {tags && (
-          <div className={s.tagsContainer}>
-            {tags.map((tag) => <Tag {...tag} />)}
-          </div>
-        )}
-      </div>
-      <Link href={url} className={s.link} aria-label={heading} />
-    </div>
-  )
+export default function Card({
+	thumbnail,
+	heading,
+	description,
+	tags,
+	url,
+	className,
+}: CardProps) {
+	return (
+		<div className={classNames(s.card, className)}>
+			<div className={s.image}>
+				<Image
+					{...thumbnail}
+					width={800}
+					height={450}
+					layout="responsive"
+					objectFit="cover"
+					placeholder="blur"
+				/>
+			</div>
+			<div className={s.body}>
+				<h2 className={s.cardTitle}>{heading}</h2>
+				{description && <p className={s.description}>{description}</p>}
+				{tags && (
+					<div className={s.tagsContainer}>
+						{tags.map((tag) => (
+							<Tag {...tag} />
+						))}
+					</div>
+				)}
+			</div>
+			<Link
+				href={url}
+				className={s.link}
+				aria-label={heading}
+				rel="noopener noreferrer"
+				target="_blank"
+			/>
+		</div>
+	);
 }
