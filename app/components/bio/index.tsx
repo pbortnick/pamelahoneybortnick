507cf7732bb4c1c626a63c51f6faeeb8c0bcacc3
Pamela Bortnick <pbortnick@gmail.com>
2025-08-22 23:23:50 -0400

    update bio

--- app/components/bio/index.tsx
+++ app/components/bio/index.tsx
@@ -5,12 +5,13 @@ const Bio = () => {
 		<div className={s.bio}>
 			<p className={s.bioText}>
 				Pamela Bortnick is a full stack software engineer with over 9 years of
-				experience. She’s passionate about delivering exceptional digital experiences and brings a sharp eye for detail, both in code and design.
+				experience. She’s passionate about delivering exceptional digital
+				experiences and brings a sharp eye for detail, both in code and design.
 			</p>
 			<p className={s.bioText}>
 				Pamela loves to learn and pick up new hobbies and skills. She spends her
-				free time seeing live music and playing with her adorable little beagle
-				named Rummi.
+				free time seeing reading, watching tv, seeing live music, and playing
+				with her 1 year old beagle named Rummi.
 			</p>
 		</div>
 	);
